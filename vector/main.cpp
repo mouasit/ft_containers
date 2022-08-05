430f5d7110c470604f98cfa57a0747c9f2af1b53
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-08-05 17:24:54 +0100

    create mini tester and value of data type

--- vector/main.cpp
+++ vector/main.cpp
@@ -3,5 +3,37 @@
 
 int main()
 {
-    ft::Vector<char> vector(5);
+    ft::Vector<char> myVector(5);
+    std::vector<char> vector(5);
+
+    std::cout << std::endl;
+
+    std::cout << "myVector :";
+
+    std::cout << std::endl<< std::endl;
+
+    std::cout << "size() : " << myVector.size();
+
+    std::cout << std::endl;
+
+    std::cout << "capacity() : " << myVector.capacity();
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "--------------------------";
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "vector :";
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "size() : " << vector.size();
+
+    std::cout << std::endl;
+
+    std::cout << "capacity() : " << vector.capacity();
+
+    std::cout << std::endl << std::endl;
+
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -15,16 +15,30 @@ namespace ft{
         public:
             typedef T       _type;
             typedef Alloc    _allocation_type;
-            
-            Vector() : _array(NULL), _size(0), _capacity(0){};
+    
+            Vector()
+                : _array(NULL), _size(0), _capacity(0){};
 
-            Vector(const int len)
-                : _array(NULL), _size(0), _capacity(0){
+            Vector(const size_t len, const T value = T())
+                : _array(NULL), _size(len), _capacity(len){
+
             this->_array = this->_allocation.allocate(len);
-
-        };;
+            for (size_t i = 0; i < len; i++)
+                this->_allocation.construct(this->_array + i, value);
+        };
             ~Vector(){};
-};
+            T operator[](int index){
+                return this->_array[index];
+            }
+
+            int size(){
+                return this->_size;
+            }
+
+            int capacity(){
+                return this->_capacity;
+            }
+    };
 }
 
 #endif

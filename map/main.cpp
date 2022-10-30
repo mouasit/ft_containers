8c0b9ae80e2e70f09b4ef18ed1eedae956a715cf
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-30 15:45:00 +0100

    add earse with leaf node

--- map/main.cpp
+++ map/main.cpp
@@ -4,11 +4,14 @@ int main(){
     Tree     myTree;
 
     myTree.insert(8);
-    std::cout << std::endl;
+    myTree.insert(6);
+    myTree.insert(7);
     myTree.insert(5);
-    std::cout << std::endl;
-    myTree.insert(2);
-    std::cout << std::endl;
-    myTree.insert(1);
-    std::cout << std::endl;
+    myTree.insert(9);
+    myTree.insert(10);
+    myTree.earse(5);
+    myTree.earse(7);
+    myTree.earse(9);
+    myTree.printTree(myTree.root);
+
 }
--- map/map.hpp
+++ map/map.hpp
@@ -7,62 +7,79 @@
 class Tree
 {
     private:
-    struct Node
-    {
+    struct Node{
         int data;
         Node *left;
         Node *right;
     };
-    
-    Node *root = nullptr;
-    public:
-        void insert(int value){
-            if (root == nullptr)
-            {
-                Node *newNode = new Node;
-                newNode->data = value;
-                newNode->left = nullptr;
-                newNode->right = nullptr;
-                root = newNode;
-                std::cout << "root" << std::endl;
-                return ;
-            }
-            else
-                insertHelper(root,value);
-        };
 
-        void    insertHelper(Node *node, int value)
+
+    Node *createNode(int value){
+        Node *newNode = new Node;
+        newNode->data = value;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        return newNode;
+    }
+
+    void    insertHelper(Node *tmp, int value)
+    {
+        if(value <= tmp->data)
         {
-            int i = 0;
-            if (value <= node->data)
-            {
-                if (node->left == nullptr)
-                {
-                    Node *newNode = new Node;
-                    newNode->data = value;
-                    newNode->left = nullptr;
-                    newNode->right = nullptr;
-                    node->left = newNode;
-                    i = 8;
-                }
-                else
-                    insertHelper(node->left,value);
-            }
+            if(tmp->left == nullptr)
+                tmp->left = createNode(value);
+            else
+                insertHelper(tmp->left, value);
+        }
+        else{
+            if(tmp->right == nullptr)
+                tmp->right = createNode(value);
+            else
+                insertHelper(tmp->right, value);
+        }
+    }
 
-            else{
-                if (node->right == nullptr)
-                {
-                    Node *newNode = new Node;
-                    newNode->data = value;
-                    newNode->left = nullptr;
-                    newNode->right = nullptr;
-                    node->left = newNode;
-                }
-                else
-                    insertHelper(node->right,value);
+    Node   *earseHelper(Node *root, int value)
+    {
+        if(root == nullptr)
+            return root;
+        if (value < root->data)
+            root->left = earseHelper(root->left,value);
+        else if (value > root->data)
+            root->right = earseHelper(root->right, value);
+        else{
+            //leaf node 
+            if (root->left == nullptr && root->right == nullptr)
+            {
+                root = nullptr;
+                delete root;
+                return nullptr;
             }
         }
-};
+        return root;
+    };
+    public:
+        Node    *root = nullptr;
+        void    insert(int value){
+            if (root == nullptr)
+                root = createNode(value);
+            else
+                insertHelper(root,value);
+        }
 
+        void  earse(int value){
+
+            this->root = earseHelper(this->root, value);
+        }
+
+        void printTree(Node *root){
+            if (root == nullptr) {
+                return;
+            } 
+            printTree(root->left);
+            std::cout << root->data << std::endl;
+            printTree(root->right);
+        };
+};
 
 #endif

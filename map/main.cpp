50f29f5e1b20d528f28402067f8d79efe87e92e1
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-11-07 18:32:23 +0100

    oraganisation of map.hpp

--- map/main.cpp
+++ map/main.cpp
@@ -1,19 +1,6 @@
 #include "map.hpp"
-#include <functional>
 
 int main(){
-    ft:: pair<int,int> pair6(5,2);
 
-    ft::avlTree<std::string, int> avlTree;
-
-    avlTree.insert(ft::pair<std::string,int>("mustapha",20));
-    avlTree.insert(ft::pair<std::string,int>("ayoub",5));
-    avlTree.insert(ft::pair<std::string,int>("hamid",2));
-
-    avlTree.printTree(avlTree.root);
-
-    avlTree.earse("ayoub");
-
-    avlTree.printTree(avlTree.root);
-
+    ft::map<int,std::string> my_map;
 }
---
+++ utils/pair.hpp
@@ -0,0 +1,30 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+template <typename T1, typename T2>
+class pair
+{
+    private:
+    public:
+    T1      first;
+    T2      second;
+    pair(void){};
+    pair(const T1 first, const T2 second){
+        this->first = first;
+        this->second = second;
+    }
+    
+    pair &operator=(const pair &pair)
+    {
+        this->first = pair.first;
+        this->second = pair.second;
+        return *this;
+    }
+    
+    pair make_pair(const T1 value1, const T2 value2)
+    {
+        pair<T1,T2> pair(value1,value2);
+        return (pair);
+    }
+};
+
+#endif

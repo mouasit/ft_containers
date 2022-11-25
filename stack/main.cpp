37653b93a10168608cd1712a8731497fbc440964
mouasit <mustaphaouassit60@gmail.com>
2022-11-25 02:19:10 +0100

    add stack with member functions

---
+++ stack/main.cpp
@@ -0,0 +1,5 @@
+#include "stack.hpp"
+
+int main(){
+    std::cout << "im in stack" << std::endl;
+}
---
+++ stack/stack.hpp
@@ -0,0 +1,61 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <iostream>
+#include "../vector/vector.hpp"
+
+namespace ft
+{
+    template <class T, class Container = ft::vector<T> > 
+    class stack{
+        public:
+            typedef T           value_type;
+			typedef Container   container_type;
+			typedef size_t      size_type;
+
+            container_type      container;
+
+            explicit stack (const container_type& ctnr = container_type()):container(ctnr){};
+        
+            stack(stack const & rhs) : container(rhs.container) {}
+			stack&		operator=(stack const & rhs)
+			{
+				this->container = rhs.container;
+				return *this;
+			}
+			~stack(){}
+            
+            bool empty() const 
+            { 
+                return container.empty();
+            }
+
+			size_type size() const
+            { 
+                return container.size();
+            }
+
+            
+			value_type& top()
+            {
+                return container.back();
+            }
+
+			const value_type& top() const
+            {
+                return container.back();
+            }
+
+			void push (const value_type& val)
+            { 
+                container.push_back(val);
+            }
+
+			void pop()
+            {
+                container.pop_back();
+            }
+    };
+}
+
+#endif 
